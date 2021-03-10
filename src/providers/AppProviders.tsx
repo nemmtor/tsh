44eb4052b882ff7cb6da6f501edd41a5495fe610
Kacper Witas <kacper0witas@gmail.com>
2021-03-10 21:10:14 +0100

    Added prettier to keep code formatting constant

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { CssBaseline } from "@material-ui/core";
-import { ThemeProvider } from "@material-ui/styles";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
 
-import { AppProvidersProps } from "./AppProviders.types";
-import { theme } from "./theme";
+import { AppProvidersProps } from './AppProviders.types';
+import { theme } from './theme';
 
 export const AppProviders = ({ children }: AppProvidersProps) => (
   <ThemeProvider theme={theme}>
