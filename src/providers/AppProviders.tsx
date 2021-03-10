39f2a3c518106d7de474b0b950fc79e14e9211cd
Kacper Witas <kacper0witas@gmail.com>
2021-03-10 20:40:15 +0100

    Added ThemeProvider and basic theme

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
 
-import { AppProvidersProps } from './AppProviders.types';
+import { AppProvidersProps } from "./AppProviders.types";
+import { theme } from "./theme";
 
-export const AppProviders = ({ children }: AppProvidersProps) => <Router>{children}</Router>;
+export const AppProviders = ({ children }: AppProvidersProps) => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Router>{children}</Router>
+  </ThemeProvider>
+);
---
+++ src/providers/theme.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createMuiTheme, ThemeProvider, CssBaseline } from '@material-ui/core';
+
+export const theme = createMuiTheme({
+  palette: {
+   primary: {
+     main: '#4460F7',
+     dark: '#2140E8',
+   },
+   secondary: {
+     main: '#F9A52B',
+   },
+  }
+})
