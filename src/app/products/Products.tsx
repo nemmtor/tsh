dd17eaa99c21e5feae1a6e4444df70573466ca19
Kacper Witas <kacper0witas@gmail.com>
2021-03-10 22:44:04 +0100

    Added reexport to navbar

--- src/app/products/Products.tsx
+++ src/app/products/Products.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { AppRoute } from 'routing/AppRoute.enum';
-import { NavBar } from 'components/NavBar/NavBar';
+import { NavBar } from 'components/NavBar';
 
 export const Products = () => {
   return (
