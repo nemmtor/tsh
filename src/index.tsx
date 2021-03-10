fe307e4b8b6ccb8bf84c70c8dc1d87f908ddf2f6
Kacper Witas <kacper0witas@gmail.com>
2021-03-10 21:09:59 +0100

    Added Nunito font

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { AppProviders } from 'providers/AppProviders';
+import 'styles/index.css';
 
 import { App } from './app/App';
 import * as serviceWorker from './serviceWorker';
--- src/providers/theme.tsx
+++ src/providers/theme.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { createMuiTheme, ThemeProvider, CssBaseline } from '@material-ui/core';
 
 export const theme = createMuiTheme({
+  typography: {
+    fontFamily: ['Nunito', 'sans-serif'].join(', '),
+  },
   palette: {
-   primary: {
-     main: '#4460F7',
-     dark: '#2140E8',
-   },
-   secondary: {
-     main: '#F9A52B',
-   },
-  }
-})
+    primary: {
+      main: '#4460F7',
+      dark: '#2140E8',
+    },
+    secondary: {
+      main: '#F9A52B',
+    },
+  },
+});
