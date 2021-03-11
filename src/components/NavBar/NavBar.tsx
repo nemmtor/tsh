22f26f4c4f800da710c426517e6642533ff4319c
Kacper Witas <kacper0witas@gmail.com>
2021-03-11 22:17:55 +0100

    Added responsive styles to navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,8 +3,7 @@ import {
   Avatar,
   makeStyles,
   Theme,
-  FormControlLabel,
-  Checkbox,
+  Container,
 } from '@material-ui/core';
 import React from 'react';
 
@@ -15,29 +14,56 @@ import { Search } from '../Search';
 import { CustomCheckbox } from '../CustomCheckbox';
 
 const useStyles = makeStyles((theme: Theme) => ({
+  wrapper: {
+    backgroundColor: '#FFF',
+  },
   navBar: {
     backgroundColor: '#FFF',
     color: theme.palette.text.primary,
-    padding: '52px 24px 32px',
+    padding: '52px 0 32px',
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     flexWrap: 'wrap',
+    [theme.breakpoints.up('sm')]: {
+      flexWrap: 'unset',
+      padding: '0 0 48px 0',
+    },
+    [theme.breakpoints.up('md')]: {
+      paddingTop: '48px',
+    },
   },
   inputsWrapper: {
     width: '100%',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
   },
   logoWrapper: {
     order: -1,
+    [theme.breakpoints.up('sm')]: {
+      order: 'unset',
+      marginRight: '50px',
+    },
   },
   searchWrapper: {
     width: '100%',
-    maxWidth: '392px',
     margin: '24px 0',
+    [theme.breakpoints.up('sm')]: {
+      maxWidth: '300px',
+      marginTop: '48px',
+    },
+    [theme.breakpoints.up('md')]: {
+      maxWidth: '392px',
+      margin: '0 24px 0 0',
+    },
   },
   avatarWrapper: {
     order: -1,
+    [theme.breakpoints.up('sm')]: {
+      order: 'unset',
+    },
   },
   avatar: {
     height: '48px',
@@ -48,20 +74,24 @@ const useStyles = makeStyles((theme: Theme) => ({
 export const NavBar = () => {
   const styles = useStyles();
   return (
-    <AppBar elevation={0} className={styles.navBar} position="static">
-      <div className={styles.logoWrapper}>
-        <Logo />
-      </div>
-      <div className={styles.inputsWrapper}>
-        <div className={styles.searchWrapper}>
-          <Search />
-        </div>
-        <CustomCheckbox name="active" label="Active" />
-        <CustomCheckbox name="promo" label="Promo" />
-      </div>
-      <div className={styles.avatarWrapper}>
-        <Avatar data-testid="avatar" src={avatar} alt="user avatar" />
-      </div>
-    </AppBar>
+    <div className={styles.wrapper}>
+      <Container>
+        <AppBar elevation={0} className={styles.navBar} position="static">
+          <div className={styles.logoWrapper}>
+            <Logo />
+          </div>
+          <div className={styles.inputsWrapper}>
+            <div className={styles.searchWrapper}>
+              <Search />
+            </div>
+            <CustomCheckbox name="active" label="Active" />
+            <CustomCheckbox name="promo" label="Promo" />
+          </div>
+          <div className={styles.avatarWrapper}>
+            <Avatar data-testid="avatar" src={avatar} alt="user avatar" />
+          </div>
+        </AppBar>
+      </Container>
+    </div>
   );
 };
